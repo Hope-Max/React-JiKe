bc6546d5b580c317a01714e85961db0a425c3b06
Hope-Max <hope_max@outlook.com>
2024-01-31 12:40:43 -0500

    Completed the encapsulation of methods related to Token

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,20 +1,21 @@
 // State management related to users - 和用户相关的状态管理
 import { createSlice } from "@reduxjs/toolkit";
 import { request } from "@/utils/request"
+import { setToken as _setToken, getToken } from "@/utils";
 
 const userStore = createSlice({
   name: "user",
   // States - 数据状态
   initialState: {
     // Token persistence - Token持久化
-    token: localStorage.getItem("token_key") || ""
+    token: getToken() || ""
   },
   // Sync actions - 同步修改方法
   reducers: {
     setToken(state, action) {
       state.token = action.payload
       // Store the token locally
-      localStorage.setItem("token_key", action.payload)
+      _setToken(action.payload)
     }
   }
 })
---
+++ src/utils/token.js
@@ -0,0 +1,13 @@
+// Encapsulate the methods of Token
+const TOKEN_KEY = "token_key"
+const setToken = (token) => {
+  localStorage.setItem(TOKEN_KEY, token)
+}
+const getToken = () => {
+  return localStorage.getItem(TOKEN_KEY)
+}
+const removeToken = () => {
+  localStorage.removeItem(TOKEN_KEY)
+}
+
+export { setToken, getToken, removeToken }
