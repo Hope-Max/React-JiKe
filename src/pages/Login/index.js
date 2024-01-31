8a79e20cdce5b2b28afc7e78e1cd895105c9e78d
Hope-Max <hope_max@outlook.com>
2024-01-30 21:52:02 -0500

    Completed the Login section

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,53 @@
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+import logo from '@/assets/logo.png'
+
 const Login = () => {
-  return <div>this is Login</div>
+
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  }
+
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* Login Form */}
+        <Form autoComplete="off" validateTrigger="onBlur" onFinish={onFinish}>
+          <Form.Item
+            name="mobile"
+            // Multiple validation logic: Validate the previous rule first, then proceed to validate the next one
+            rules={[
+              {
+                required: true,
+                message: 'Please input your phone number!',
+              },
+              {
+                pattern: /^1[3-9][0-9]{9}$/,
+                message: "Please enter the correct format for the phone number."
+              }
+            ]}>
+            <Input size="large" placeholder="Phone Number" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[
+              {
+                required: true,
+                message: 'Please input the verification code!',
+              },
+            ]}>
+            <Input size="large" placeholder="Verification Code" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
 
 export default Login
